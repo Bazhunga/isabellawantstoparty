a24efb816cb1fcb84ee86ea737118dbac30cb375
Kevin <zhu.s.kevin@gmail.com>
2015-05-07 02:51:53 -0400

    done ish

---
+++ clicker.js
@@ -0,0 +1,9 @@
+function changeBackground(option){
+	// var image = document.querySelector(".fullbody");
+	// if (option === 'yes'){
+	// 	image.style.backgroundImage = "url('images/party.jpg')";
+	// }
+	// else if (option === 'no'){
+	// 	image.style.backgroundImage = "url('images/noparty.JPG')";
+	// }
+}
